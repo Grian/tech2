cede7c05957e76d12314a6030d2fe89e8ff89ee2
Anatoliy Grishayev <grian@cpan.org>
2016-03-15 11:34:32 +0300

    boiler plate stepic

---
+++ boil.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p etc public/{img,css,js} uploads/
+touch etc/nginx.conf
+touch etc/gunicorn.conf
+touch  init.sh
+chmod 0755 init.sh
+
+NAME=webtech.conf
+echo '#!/bin/bash' >> init.sh
+echo "NAME=$NAME" >> init.sh
+echo 'sudo ln -fs $PWD/etc/nginx.conf /etc/nginx/sites-enabled/$NAME' >> init.sh
+echo 'sudo /etc/init.d/nginx restart' >> init.sh
+echo 'sudo ln -fs $PWD/etc/gunicorn.conf /etc/gunicorn.d/$NAME' >> init.sh
+echo 'sudo /etc/inid.d/gunicorn restart' >> init.sh
