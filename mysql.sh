7aca3df6754fd6a90f5504dbdca09a9ca1f21cbb
Anatoliy Grishayev <grian@cpan.org>
2016-03-16 23:03:16 +0300

    1

--- mysql.sh
+++ mysql.sh
@@ -4,4 +4,4 @@ mysql -uroot mysql -e 'create user tech'
 mysql -uroot mysql -e 'grant all on tech.* to tech'
 mysql -uroot mysql -e 'alter database tech character set utf8'
 cd ask
-./manage syncdb
+./manage.py syncdb
