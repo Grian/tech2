0073f4f5e02df9f40bc1c3ad5516e90b579d34ad
Anatoliy Grishayev <grian@cpan.org>
2016-03-16 23:17:10 +0300

    1

--- boil.sh
+++ boil.sh
@@ -12,7 +12,7 @@ NAME=webtech.conf
 echo '#!/bin/bash' >> init.sh
 echo "NAME=$NAME" >> init.sh
 echo 'sudo ln -fs $PWD/etc/nginx.conf /etc/nginx/sites-enabled/$NAME' >> init.sh
-echo 'sudo rm /etc/nginx/sites-enabled/default' >> init.sh
+#echo 'sudo rm /etc/nginx/sites-enabled/default' >> init.sh
 echo 'sudo /etc/init.d/nginx restart' >> init.sh
 echo 'sudo ln -fs $PWD/etc/gu-hello.py /etc/gunicorn.d/gu-hello.py' >> init.sh
 echo 'sudo ln -fs $PWD/etc/gu-ask.py /etc/gunicorn.d/gu-ask.py' >> init.sh
